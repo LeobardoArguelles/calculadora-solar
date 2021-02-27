f1da3fe285c43486d682ac907c73bb7bc95fadab
Leobardo Arguelles <leobardomed@gmail.com>
2021-02-26 21:00:16 -0800

    Add maps to landing page

---
+++ src/tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontSize: {
+        '3xl': '1.875rem',
+        '4xl': '2.25rem',
+        '5xl': '3rem',
+        '6xl': '4rem',
+        '7xl': '5rem',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
